fbfff88a533cad802fa4bf78e599a6fb318753ec
b-ellis <bradjellis18@gmail.com>
2016-06-28 22:44:17 -0700

    adding items function added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-	$('.items').append($('.cart-items'));
-})
+	 $('.submit').on("click", function(event){
+	 	event.preventDefault();
+	 	var item = $('.searchbar').val();
+	 	$('.items').append('<li><i class="fa fa-check"><i class="fa fa-trash">' + item + '</li>');
+	 	console.log(this);
+	 });
+	 $('.fa-check').click(function() {
+	 	var currentItem = $(this).parent('li');
+	 	currentItem.appendTo('.cart .cart-items');
+	 	console.log(this);
+	 });
+	 $('.fa-trash').on('click', function() {
+	 	console.log(this);
+	 	var currentItem = $(this).parent('li');
+	 	currentItem.remove();
+	 });
+
+});
